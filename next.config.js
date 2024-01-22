80a912f8fe1e6ed15ea83595955fc8e973c7d33e
sonnt <sonnt@toprate.io>
2024-01-22 16:04:15 +0700

    fix: build next

--- next.config.js
+++ next.config.js
@@ -5,33 +5,29 @@ const nextConfig = {
     //   locales: ["en", "vi", "ja"],
     //   defaultLocale: "en",
     // },
-  // images: {
-  //   minimumCacheTTL: 60,
-  //   remotePatterns: [
-  //     // {
-  //     //   protocol: 'https',
-  //     //   hostname: 'api.asiavivatravel.com',
-  //     // },
-  //     // {
-  //     //   protocol: 'https',
-  //     //   hostname: 'd2czpmgoouh0j8.cloudfront.net'
-  //     // },{
-  //     //   protocol: 'https',
-  //     //   hostname: 'asia-vivatravel.s3.eu-west-2.amazonaws.com'
-  //     // }
-  //     {
-  //       protocol: 'https',
-  //       hostname: '*',
-  //     },
-  //   ],
-  //   // formats: ['image/avif', 'image/webp'],
-  // },
   images: {
-    loader: 'custom',
-    loaderFile: './loader.js'
+    minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.asiavivatravel.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd2czpmgoouh0j8.cloudfront.net'
+      },{
+        protocol: 'https',
+        hostname: 'asia-vivatravel.s3.eu-west-2.amazonaws.com'
+      }
+    ],
+    formats: ['image/avif', 'image/webp'],
   },
+  // images: {
+  //   loader: 'custom',
+  //   loaderFile: './loader.js'
+  // },
   staticPageGenerationTimeout: 1000,
-  output: 'standalone',
+  // output: 'standalone',
 }
 
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
